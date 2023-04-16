2e50e603bef482357e6022bbbb6cd4d67e805070
mneme07 <mjneme1995@gmail.com>
2023-04-15 23:49:14 -0300

    Products and cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -10,10 +10,16 @@ const cartRouter = Router();
 const cartManager = new FileManager("./db/cart.json");
 const cartFileManager = new CartFileManager("./db/cart.json");
 
-cartRouter.get("/", async (req, res) => {
-    const limit = parseInt(req.query.limit);
-    const products = await productsManager.getsProducts();
-    res.send(limit ? products.slice(0, limit) : products);
+cartRouter.post("/", async (req, res) => {
+    try {
+        const element ={"products":[]}
+        const cart = await cartFileManager.addElement(element);
+        res.send(`Carrito creado id:${cart}`);
+    } catch (error) {
+        res.status(400).send().json({
+            error: error
+        });
+    }
 });
 
 cartRouter.get("/:cid", async (req, res) => {
@@ -26,17 +32,6 @@ cartRouter.get("/:cid", async (req, res) => {
     }
 });
 
-cartRouter.post("/", async (req, res) => {
-    try {
-        const element ={"products":[]}
-        const cart = await cartFileManager.addElement(element);
-        res.send(`Carrito creado id:${cart}`);
-    } catch (error) {
-        res.status(400).send().json({
-            error: error
-        });
-    }
-});
 
 cartRouter.post("/:cid/product/:pid", async (req, res) => {
     try {
@@ -50,27 +45,5 @@ cartRouter.post("/:cid/product/:pid", async (req, res) => {
         });
     }
 });
-
-cartRouter.put("/:pid", async (req, res) => {
-    try {
-        const pid = parseInt(req.params.pid);
-        const elements = req.body;
-        const products = await cartManager.update(pid,elements);
-        res.send(products);
-    } catch (error) {
-        res.status(error.details.code   ).json(error);
-    }
-});
-
-cartRouter.delete("/:pid", async (req, res) => {
-    try {
-        const pid = parseInt(req.params.pid);
-        const products = await cartManager.delete(pid);
-        res.send(products);
-    } catch (error) {
-        res.status(error.details.code).json(error);
-    }
-});
-
 
 export default cartRouter;
