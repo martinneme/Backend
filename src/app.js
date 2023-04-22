1f4335b2b484236b4bf40b50b19e9c6d17ac01ce
mneme07 <mjneme1995@gmail.com>
2023-04-22 01:09:03 -0300

    plantillas

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import productsRouter from './routes/products.js'
 import cartRouter from './routes/cart.js';
+import handlebars  from 'express-handlebars';
+import __dirname from './utils.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(`${__dirname}/public`));
 
-app.use(express.static('public'))
 
-app.use('/api/products',productsRouter)
+
+app.engine('handlebars',handlebars.engine());
+app.set('views',`${__dirname}/views` ); 
+app.set('view engine',`handlebars` ); 
+
+
+app.use('/',productsRouter)
 app.use('/api/carts',cartRouter)
+
 
 
 app.listen(8080,()=>{
--- src/routes/products.js
+++ src/routes/products.js
@@ -9,26 +9,19 @@ const productsRouter = Router();
 const productsManager = new FileManager("./db/products.json");
 
 productsRouter.get("/", async (req, res) => {
-    const limit = parseInt(req.query.limit);
     const products = await productsManager.getsProducts();
-    res.send(limit ? products.slice(0, limit) : products);
-});
 
-productsRouter.get("/:pid", async (req, res) => {
-    try {
-        const pid = parseInt(req.params.pid);
-        const product = await productsManager.getProductById(pid);
-        res.send(product);
-    } catch (error) {
-        res.status(404).send(error);
-    }
+    res.render('home',{products})
 });
 
 productsRouter.post("/", addProductValidator, async (req, res) => {
     try {
         const element = req.body;
         const products = await productsManager.addElement(element);
-        res.send(`Producto agregado id:${products}`);
+        if(products){
+            res.send(`Producto agregado id:${products}`); 
+        }
+       
     } catch (error) {
         res.status(400).send().json({
             error: error
@@ -36,26 +29,41 @@ productsRouter.post("/", addProductValidator, async (req, res) => {
     }
 });
 
-productsRouter.put("/:pid", async (req, res) => {
-    try {
-        const pid = parseInt(req.params.pid);
-        const elements = req.body;
-        const products = await productsManager.update(pid,elements);
-        res.send(products);
-    } catch (error) {
-        res.status(error.details.code   ).json(error);
-    }
-});
 
-productsRouter.delete("/:pid", async (req, res) => {
-    try {
-        const pid = parseInt(req.params.pid);
-        const products = await productsManager.delete(pid);
-        res.send(products);
-    } catch (error) {
-        res.status(error.details.code).json(error);
-    }
-});
+
+
+// // productsRouter.get("/:pid", async (req, res) => {
+// //     try {
+// //         const pid = parseInt(req.params.pid);
+// //         const product = await productsManager.getProductById(pid);
+// //         res.send(product);
+// //     } catch (error) {
+// //         res.status(404).send(error);
+// //     }
+// // });
+
+
+
+// productsRouter.put("/:pid", async (req, res) => {
+//     try {
+//         const pid = parseInt(req.params.pid);
+//         const elements = req.body;
+//         const products = await productsManager.update(pid,elements);
+//         res.send(products);
+//     } catch (error) {
+//         res.status(error.details.code   ).json(error);
+//     }
+// });
+
+// productsRouter.delete("/:pid", async (req, res) => {
+//     try {
+//         const pid = parseInt(req.params.pid);
+//         const products = await productsManager.delete(pid);
+//         res.send(products);
+//     } catch (error) {
+//         res.status(error.details.code).json(error);
+//     }
+// });
 
 
 export default productsRouter;
